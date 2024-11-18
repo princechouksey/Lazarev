28535eb658bf526ffd8a10faeadd57872acd9160
Prince Chouksey <Princechouksey179@gmail.com>
2024-11-18 23:38:48 +0530

    page4 done

--- script.js
+++ script.js
@@ -40,4 +40,60 @@ function navAnimation() {
     })
 }
 
-navAnimation()
+// navAnimation()
+
+var rightElems = document.querySelectorAll(".right-elem");
+// var relemImg = document.querySelector("#right-elem1 img");
+// relem.addEventListener( "mouseenter",function(){
+//     relemImg.style.opacity = 1;
+
+// })
+ function page2Animation(){
+    rightElems.forEach(function(elem){
+        elem.addEventListener("mouseenter" , function(){
+           gsap.to( elem.childNodes[3],{
+             opacity:1,
+             scale:1
+           })
+        })
+        elem.addEventListener("mouseleave" , function(){
+         gsap.to( elem.childNodes[3],{
+             opacity:0,
+             scale:0
+           })
+        })
+        elem.addEventListener("mousemove", function(dets){
+         gsap.to( elem.childNodes[3],{
+             x:dets.x -elem.getBoundingClientRect().x-50,
+             y:dets.y -elem.getBoundingClientRect().y-200,
+         })
+        })
+     
+     })
+
+}
+// page2Animation()
+
+
+function page3VideoAnimation() {
+    var page3Center = document.querySelector(".page3-center")
+    var video = document.querySelector("#page3 video")
+
+    page3Center.addEventListener("click", function () {
+        video.play()
+        gsap.to(video, {
+            transform: "scaleX(1) scaleY(1)",
+            opacity: 1,
+            borderRadius: 0
+        })
+    })
+    video.addEventListener("click", function () {
+        video.pause()
+        gsap.to(video, {
+            transform: "scaleX(0.7) scaleY(0)",
+            opacity: 0,
+            borderRadius: "30px"
+        })
+    })
+}
+    page3VideoAnimation()
